f02792fee4100a01415c709af4a4701eb063672e
mmifs <85654339+mmifs@users.noreply.github.com>
2021-09-12 16:40:29 -0400

    sql db files working

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES
+    ('Engineering'),
+    ('Legal'),
+    ('Sales'),
+    ('Finance');
+
+INSERT INTO emp_roles (title, salary, department_id)
+VALUES
+    ('Software Engineer', 120000, 1),
+    ('Lead Engineer', 150000, 1),
+    ('Legal Team Lead', 250000, 2),
+    ('Lawyer', 190000, 2),
+    ('Accountant', 125000, 4),
+    ('Sales Lead', 100000, 3),
+    ('Salesperson', 80000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Jane', 'Doe', 2, NULL),
+    ('Jeff', 'Smith', 3, NULL),
+    ('Geoff', 'Johnson', 4, NULL),
+    ('Michael', 'Smith', 5, NULL),
+    ('Carol', 'Smith', 6, NULL),
+    ('Alex', 'Samuels', 7, NULL);
